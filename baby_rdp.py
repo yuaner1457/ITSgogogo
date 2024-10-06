1530726d322b12eaf900573d678ac4b36b52c0ac
yuaner <yuaner1942@outlook.com>
2024-10-06 13:30:41 +0800

    RDP航迹简化

--- baby_rdp.py
+++ baby_rdp.py
@@ -1,15 +1,9 @@
-import numpy as np
 import pandas as pd
 from rdp import rdp
-import ast
+import Baby_washdata_change as wdc
+import numpy as np
 
-def clean(row,axis):
 
-        # 解析字符串为列表
-    result =  np.fromstring(row,sep=' ')
-        # 转换为 NumPy 数组
-    result = np.array(result)
-    return result
 def calculate_deviation(original_path, simplified_path):
     deviations = []
     for point in original_path:
@@ -19,16 +13,14 @@ def calculate_deviation(original_path, simplified_path):
     # 返回平均偏离度
     return np.mean(deviations)
 
-def simplify(x):
-    data=x.to_numpy()
-    data_simple=rdp(data,1)
-    print(data_simple)
-    return calculate_deviation(data,data_simple)
+def simplify(x,epsilon):
+    data=x['coordinate']
+    data_simple=rdp(data,epsilon)
+    x['coordinate']=data_simple
+    return x
 
 if __name__ == '__main__':
-    data=pd.read_csv('washed_data_50_1.csv')
-    data=data['coordinate']
-    data=data.apply(clean,axis=1)
-    test=ast.literal_eval('[[1,2,3],[4,6,5]]')
-    x=simplify(data)
-    print(x)
+    data=wdc.trans_data("washed_data_50_1.csv")
+    epsilon=1
+    data=data.apply(lambda row: simplify(row,epsilon),axis=1)
+    print(data)
