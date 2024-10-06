9094ee1ad0057decb863b5cf7eca0cfdaa64f786
yuaner <yuaner1942@outlook.com>
2024-10-06 12:36:59 +0800

    ARI判断函数

--- Baby_ARI.py
+++ Baby_ARI.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import adjusted_rand_score
 
-#接受的数据类型是两个list
+#接受的数据类型是两个list，其中x是咱们的模型聚类出来的标签，y是实际的label，输出在（0,1），越靠近1越好
 def ARI(x,y):
     if isinstance(x,list) and isinstance(y,list):
         return adjusted_rand_score(x,y)
