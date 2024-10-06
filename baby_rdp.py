c4e1b15c65db23945595845bb618a895fb2ea7a5
yuaner <yuaner1942@outlook.com>
2024-10-06 14:03:39 +0800

    RDP路径简化算法：包括测试但是不包括结果

--- baby_rdp.py
+++ baby_rdp.py
@@ -5,13 +5,15 @@ import numpy as np
 
 
 def calculate_deviation(original_path, simplified_path):
-    deviations = []
+    original_path=original_path.loc['coordinate']
+    simplified_path=simplified_path.loc['coordinate']
+    deviations = 0
     for point in original_path:
         # 计算每个点到简化路径的最小距离
         min_distance = np.min(np.sqrt(np.sum((simplified_path - point) ** 2, axis=1)))
-        deviations.append(min_distance)
+        deviations=deviations+min_distance
     # 返回平均偏离度
-    return np.mean(deviations)
+    return deviations
 
 def simplify(x,epsilon):
     data=x['coordinate']
@@ -20,7 +22,13 @@ def simplify(x,epsilon):
     return x
 
 if __name__ == '__main__':
-    data=wdc.trans_data("washed_data_50_1.csv")
-    epsilon=1
-    data=data.apply(lambda row: simplify(row,epsilon),axis=1)
-    print(data)
+    data_init=wdc.trans_data("washed_data_50_1.csv")
+    see=[]
+    for i in np.arange(1,0,-0.001):
+        epsilon=i
+        data=data_init.apply(lambda row: simplify(row,epsilon),axis=1)
+        loss_num=data_init.apply(lambda row: calculate_deviation(row,data.loc[row.name]),axis=1)
+        loss_num=np.mean(loss_num)
+        see.append(loss_num)
+    see=pd.DataFrame(see,columns=['See'])
+    see.to_excel('see.xlsx')
