278740afc185a0f6cdffdbf6ae40f9fccd014cb1
yuaner <yuaner1942@outlook.com>
2024-10-06 20:37:06 +0800

    需要运行的文件-生成损失矩阵

--- Baby_creat_dtwmartrix.py
+++ Baby_creat_dtwmartrix.py
@@ -3,13 +3,17 @@ import pandas as pd
 from DTW import DTW
 from sklearn.metrics import pairwise_distances
 import Baby_washdata_change as wsd
+from scipy.spatial.distance import pdist, squareform
+
 
 
 def main():
     data=wsd.trans_data('simple_path(1).csv')
     flight=data['coordinate']
     flight=flight.tolist()
-    distance_matrix = pairwise_distances(flight, metric=DTW)
+    flight=np.array(flight,dtype=object)
+    distance_matrix = pdist(flight,metric=DTW)
+    distance_matrix=squareform(distance_matrix)
     distance_matrix=pd.DataFrame(distance_matrix)
     distance_matrix.to_excel('distance_matrix.xlsx')
     return
